b91f6991c28639139273ba2a8af07855210cb989
zhangjiacheng1997 <286801343@qq.com>
2019-03-19 15:19:10 +0800

    ko

--- rectangle.js
+++ rectangle.js
@@ -1,67 +1,18 @@
-$(function(){
-  var $width = $('#rectangle-width'),
-      $height = $('#rectangle-height'),
-      $calc = $('#rectangle-calc'),
-      blnValid = false;
+/* global Rectangle: true */
+$(function() {
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
 
-$calc.click(function{
-  if(blnValid)return;
+  $btnCal.click(function(){
+    var w = $width.val(),
+        h = $height.val();
 
-  var width = Number($width.val()),
-      height =  Number($height.val()),
-      p = roundFractional(width * 2 + height * 2, 2),
-      a = roundFractional(width * height, 2);
-      $('#rectangle-perimeter').val(p);
-      $('#rectangle-area').val(a);
-          
+    var r = new Rectangle(w, h);
+
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
+  });
 });
-$('#rectangle-perimeter').val(p);
-$('#rectangle-area').val(a);
-});
-
-$width.focusout(function() {
-      blnValid = validate('#rectangle-width');
-        
-});
-  
-$height.focusout(function() {
-      blnValid = validate('#rectangle-height');
-        
-});
-
-
-function roundFractional(x, n) {
-      return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-        
-}
-
-
-function validate(field) {
-      var $data = $(field),
-      $message = $(field + '-validate'),
-      label = $(field).attr('data-label');
-
-      if($data.val() === '') {
-          $message.html(label + '不能为空！');
-          $data.select();
-          return false;                           
-      }
-
-      if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())) {
-          $message.html(label + '必须是数值');
-          $data.select();
-          return false;                 
-      }
-
-      if(window.Number($data.val()) < 0) {
-         $message.html(label + '必须大于零');
-         $data.select();
-         return false;
-                              
-      }
-
-      $message.html('');
-      return true;
-}
-});
-
